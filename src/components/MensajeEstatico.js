dfcea6b0683b007df7c86695119cd2fdcf36ad71
Javier <mayorgas1996@correo.ugr.es>
2020-06-30 18:02:28 +0200

    Creación de componente Mensaje Estatico

---
+++ src/components/MensajeEstatico.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+const MensajeEstatico = () => {
+    return (
+        <h1>Hola soy el MensajeEstatico!</h1>
+    )
+}
+
+export default MensajeEstatico;
--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -1,10 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import MensajeEstatico from './MensajeEstatico'
 
 class Peliculas extends Component {
-  render(){
-      return(
-          <h4>Soy el componente de peliculas</h4>
-      );
+  render() {
+    return (
+      <div id="peliculas">
+        <h4>Soy el componente de peliculas</h4>
+        <MensajeEstatico />
+      </div>
+    );
   }
 }
 
