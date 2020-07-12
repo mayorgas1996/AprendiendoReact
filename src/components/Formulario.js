dda28d674b94a03be8f0de8421ea3ce4c5493711
Javier <mayorgas1996@correo.ugr.es>
2020-07-12 10:12:17 +0200

    Creación de Página para el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,17 +2,98 @@ import React, { Component } from 'react';
 import Slider from './Slider';
 import Sidebar from './Sidebar';
 
-class Home extends Component {
+class Formulario extends Component {
+
+    nombreRef = React.createRef();
+    apellidosRef = React.createRef();
+    generoHombreRef = React.createRef();
+    generoMujerRef = React.createRef();
+    generoOtroRef = React.createRef();
+    bioRef = React.createRef();
+
+    state = {
+        user: {}
+    };
+
+    recibirFormulario = (e) => {
+        e.preventDefault(); //Para que no se recargue la pantalla cuando se envíe el formulario
+
+        var genero = "hombre";
+
+        if(this.generoHombreRef.current.checked){
+            genero = this.generoHombreRef.current.value;
+        }
+        else if(this.generoMujerRef.current.checked){
+            genero = this.generoMujerRef.current.value;
+        }
+        else{
+            genero = this.generoOtroRef.current.value;
+        }
+
+        var user = {
+            nombre: this.nombreRef.current.value,
+            apellidos: this.apellidosRef.current.value,
+            bio: this.bioRef.current.value,
+            genero: genero
+        }
+
+        this.setState({
+            user: user
+        });
+
+    }
+    
     render(){
+
+        if(this.state.user.nombre){
+            var user = this.state.user;
+        }
+
         return(
             <div id="formulario">
-                <Slider
-                    title="Formulario"
-                    size="slider-small"
-                />
+
                 <div className="center">
 
                     <div id="content">
+
+                        <h1 className="subheader">Formulario</h1>
+
+                        {/*Mostrar datos del formulario*/}
+                        {
+                            this.state.user.nombre &&
+                                <div id="user-data">
+                                    <p>Nombre: <strong>{user.nombre}</strong></p>
+                                    <p>Apellidos: <strong>{user.apellidos}</strong></p>
+                                    <p>Bio: <strong>{user.bio}</strong></p>
+                                    <p>Género: <strong>{user.genero}</strong></p>
+                                </div>
+
+                        }
+
+                        <form className="mid-form" onSubmit={this.recibirFormulario} onChange={this.recibirFormulario}>
+                            <div className="form-group">
+                                <label htmlFor="nombre">Nombre</label>
+                                <input type="text" name="nombre" ref={this.nombreRef}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="apellidos">Apellidos</label>
+                                <input type="text" name="apellidos" ref={this.apellidosRef}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="bio">Biografía</label>
+                                <textarea name="bio" ref={this.bioRef}/>
+                            </div>
+                            <div className="form-group radiobuttons">
+                                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
+                                <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef}/> Mujer
+                                <input type="radio" name="genero" value="otro" ref={this.generoOtroRef}/> Otro
+                            </div>
+
+                            <div className="clearfix"></div>
+
+                            <input type="submit" value="Enviar" class="btn btn-success"/>
+
+                        </form>
                     </div>
 
                     <Sidebar 
@@ -26,4 +107,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default Formulario;
